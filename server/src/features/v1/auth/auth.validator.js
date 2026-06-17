cd4381d6a9bf8f58aa371819023413a4aeb12aa9
RezaGhs <rezaghasemi7123@gmail.com>
2026-06-17 12:26:47 +0330

    added confirm password and custom validation for it

--- server/src/features/v1/auth/auth.validator.js
+++ server/src/features/v1/auth/auth.validator.js
@@ -4,9 +4,14 @@ exports.registerSchema = z
   .object({
     email: z.email(),
     username: z.string().min(4).max(32),
-    password: z.string().min(8).max(32),
     name: z.string().min(3).max(48),
     age: z.number().min(7).max(120),
     biography: z.string().min(1).max(100).optional(),
+    password: z.string().min(8).max(32),
+    confirmPassword: z.string().min(8).max(32),
+  })
+  .refine((body) => body.confirmPassword === body.password, {
+    error: "Confirm Password is Not Equal to Password",
+    path: ["confirmPassword"],
   })
   .strict();
